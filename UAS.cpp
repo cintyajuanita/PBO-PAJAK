227b3d90a0129adba1ead5113e1209f4dcb8e4a5
cintyajuanita <134785720+cintyajuanita@users.noreply.github.com>
2023-06-13 13:06:25 +0700

    Update UAS.cpp

--- UAS.cpp
+++ UAS.cpp
@@ -1,88 +1,80 @@
 #include <iostream>
-#include <math.h>
 #include <conio.h>
 
 using namespace std;
-
-
 
 class Barang {
 private:
     string namaBrg;
     int hargaBrg;
     int ongkirBrg;
+
 public:
-    //constructor barang
-    Barang(){
+    Barang() {
         namaBrg = "";
         hargaBrg = 0;
-        ongkirBrg = 0;   
+        ongkirBrg = 0;
     }
 
-    void setBarang(){
-        cout <<"\t\t+------------------------------------------+"<<endl;
-        cout <<"\t\t|               DATA BARANG                |"<<endl;
-        cout <<"\t\t+------------------------------------------+"<<endl;
+    Barang(string nama, int harga, int ongkir) {
+        namaBrg = nama;
+        hargaBrg = harga;
+        ongkirBrg = ongkir;
+    }
+
+    void setBarang() {
+        cout << "\t\t+------------------------------------------+" << endl;
+        cout << "\t\t|               DATA BARANG                |" << endl;
+        cout << "\t\t+------------------------------------------+" << endl;
         cout << "\t\t Masukan Nama Barang     : ";
-        cin  >> namaBrg;
+        cin >> namaBrg;
     }
 
-    void setHarga(){
+    void setHarga() {
         cout << "\t\t Masukan Harga Barang    : ";
-        cin  >> hargaBrg;
+        cin >> hargaBrg;
         cout << "\t\t Masukan Ongkos Kirim    : ";
-        cin  >> ongkirBrg;
-        cout <<"\t\t+------------------------------------------+"<<endl;
+        cin >> ongkirBrg;
+        cout << "\t\t+------------------------------------------+" << endl;
         cout << endl;
     }
 
     string getNamaBarang() {
-        
         return namaBrg;
     }
-    // 
+
     int getHargaBarang() {
         return hargaBrg;
     }
-    
+
     int getHargaOngkir() {
         return ongkirBrg;
     }
 };
 
-class Pajak
-{
+class Pajak {
 private:
     int pajakLokal;
     int cukai;
     int pajakInter;
 
-public: 
-
-    // Method Hitung Pajak Lokal
-    int setPajakLokal(Barang barang)
-    {
+public:
+    int setPajakLokal(Barang barang) {
         pajakLokal = 0.11 * barang.getHargaBarang();
         return pajakLokal;
     }
 
-    // Method Hitung Bea Cukai
-    int setCukai(Barang barang)
-    {
+    int setCukai(Barang barang) {
         cukai = 0.06 * (barang.getHargaBarang() + barang.getHargaOngkir());
         return cukai;
     }
 
-    // Method Hitung Pajak Inter
-    int setPajakInter(Barang barang)
-    {
+    int setPajakInter(Barang barang) {
         pajakInter = 0.10 * barang.getHargaBarang();
         return pajakInter;
     }
 
-    // Menampilkan Detail harga dan Pajak Lokal untuk Barang User
-    void getPajakLokal(Barang barang)
-    {
+    void getPajakLokal(Barang barang) {
         cout << "\t\tNama Barang        : " << barang.getNamaBarang() << endl;
         cout << "\t\t================Pajak Lokal================" << endl;
         cout << "\t\tHarga Barang       : Rp. " << barang.getHargaBarang() << endl;
@@ -94,9 +86,7 @@ public:
         cout << endl;
     }
 
-    // Menampilkan Detail harga, cukai dan Pajak inter untuk barang User
-    void getPajakInter(Barang barang)
-    {
+    void getPajakInter(Barang barang) {
         cout << "\t\tNama Barang        : " << barang.getNamaBarang() << endl;
         cout << "\t\t============Pajak Internasional============" << endl;
         cout << "\t\tHarga Barang       : Rp. " << barang.getHargaBarang() << endl;
@@ -111,70 +101,81 @@ public:
     }
 };
 
-class Pembeli{
-    public:
-        string namaPembeli;
-        string notelp;
-        string alamat;
-    
-    void setPembeli(){
-        cout <<"\t\t+------------------------------------------+"<<endl;
-        cout <<"\t\t|               DATA PEMBELI               |"<<endl;
-        cout <<"\t\t+------------------------------------------+"<<endl;
-        cout <<"\t\t Masukan nama pembeli    : "; cin >> namaPembeli;
-        cout <<"\t\t Masukan no telp         : "; cin >> notelp;
-        cout <<"\t\t Masukan alamat          : "; cin >> alamat;
-        cout <<"\t\t+-------------------------------------------"<<endl;
+class Pembeli {
+public:
+    string namaPembeli;
+    string notelp;
+    string alamat;
+
+    void setPembeli() {
+        cout << "\t\t+------------------------------------------+" << endl;
+        cout << "\t\t|               DATA PEMBELI               |" << endl;
+        cout << "\t\t+------------------------------------------+" << endl;
+        cout << "\t\t Masukan nama pembeli    : ";
+        cin >> namaPembeli;
+        cout << "\t\t Masukan no telp         : ";
+        cin >> notelp;
+        cout << "\t\t Masukan alamat          : ";
+        cin >> alamat;
+        cout << "\t\t+-------------------------------------------" << endl;
     }
 
-    void getPembeli(){
+    void getPembeli() {
         cout << "\t\t===========================================" << endl;
-        cout << "\t\tNama pembeli       : " << namaPembeli <<endl;
-        cout << "\t\tNo. Telp           : " << notelp <<endl;
-        cout << "\t\tAlamat             : " << alamat <<endl;
-        cout << "\t\t===========================================" << endl;
-    }
-};
-
-class Penjual{
-    public:
-        string namaPenjual;
-        string notelp;
-        string alamat;
-    
-    void setPenjual(){
-        cout <<"\t\t+------------------------------------------+"<<endl;
-        cout <<"\t\t|               DATA PENJUAL               |"<<endl;
-        cout <<"\t\t+------------------------------------------+"<<endl;
-        cout <<"\t\t Masukan nama Penjual    : "; cin >> namaPenjual;
-        cout <<"\t\t Masukan no telp         : "; cin >> notelp;
-        cout <<"\t\t Masukan alamat          : "; cin >> alamat;
-        cout <<"\t\t+-------------------------------------------"<<endl;
-    }
-
-    void getPenjual(){
-        cout << "\t\tNama Penjual       : " << namaPenjual <<endl;
-        cout << "\t\tNo. Telp           : " << notelp <<endl;
-        cout << "\t\tAlamat             : " << alamat <<endl;
+        cout << "\t\tNama pembeli       : " << namaPembeli << endl;
+        cout << "\t\tNo. Telp           : " << notelp << endl;
+        cout << "\t\tAlamat             : " << alamat << endl;
         cout << "\t\t===========================================" << endl;
     }
 };
 
-void judul(){
-    cout << "\x1B[36m     _____ ____ ____ _____ ___ _____ ___  __  _____ _____    __ ___   _____\033[0m\t\t"<<endl;
-    cout << "\x1B[36m    / _  // _ //__ // ___/ _ // _  //_  |/ / / _  // _  //_ / // _ | / //_/\033[0m\t\t"<<endl;
-    cout << "\x1B[36m   / ___/ , _// _/ / (_ / , _/ __ |/ /|_/ / / ___/ __  / __/ // __ |/ ,<\033[0m\t\t"<< endl;   
-    cout << "\x1B[36m  /_/  /_/|_|/____//___/_//_/_/ |_/_/  /_/ /_/  /_/ |_//____// |_/_/__|_|\033[0m\t\t"<<endl;
-    cout <<"\x1B[36m++=======================================================================++\033[0m\t\t"<<endl;
+class Penjual {
+public:
+    string namaPenjual;
+    string notelp;
+    string alamat;
+
+    void setPenjual() {
+        cout << "\t\t+------------------------------------------+" << endl;
+        cout << "\t\t|               DATA PENJUAL               |" << endl;
+        cout << "\t\t+------------------------------------------+" << endl;
+        cout << "\t\t Masukan nama Penjual    : ";
+        cin >> namaPenjual;
+        cout << "\t\t Masukan no telp         : ";
+        cin >> notelp;
+        cout << "\t\t Masukan alamat          : ";
+        cin >> alamat;
+        cout << "\t\t+-------------------------------------------" << endl;
+    }
+
+    void getPenjual() {
+        cout << "\t\tNama Penjual       : " << namaPenjual << endl;
+        cout << "\t\tNo. Telp           : " << notelp << endl;
+        cout << "\t\tAlamat             : " << alamat << endl;
+        cout << "\t\t===========================================" << endl;
+    }
+};
+
+void judul() {
+    cout << "\x1B[36m     _____ ____ ____ _____ ___ _____ ___  __  _____ _____    __ ___   _____\033[0m\t\t" << endl;
+    cout << "\x1B[36m    / _  // _ //__ // ___/ _ // _  //_  |/ / / _  // _  //_ / // _ | / //_/\033[0m\t\t" << endl;
+    cout << "\x1B[36m   / ___/ , _// _/ / (_ / , _/ __ |/ /|_/ / / ___/ __  / __/ // __ |/ ,<\033[0m\t\t" << endl;
+    cout << "\x1B[36m  /_/  /_/|_|/____//___/_//_/_/ |_/_/  /_/ /_/  /_/ |_//____// |_/_/__|_|\033[0m\t\t" << endl;
+    cout << "\x1B[36m++=======================================================================++\033[0m\t\t" << endl;
 }
 
 int main()
 {
-    
-    Barang barang;
+
+    const int MAX_BARANG = 10;
+    Barang daftarBarang[MAX_BARANG];
+    int jumlahBarang = 0;
+
     Pajak pajak;
     Pembeli pembeli;
     Penjual penjual;
+        int totalHarga = 0; // Menambahkan variabel totalHarga
+
 
     int pil;
     system("cls");
@@ -186,18 +187,25 @@ int main()
     penjual.setPenjual();
     system("cls");
     judul();
-    barang.setBarang();
-    barang.setHarga();
+
+    // Menambahkan data barang
+    cout << "\t\tMasukkan jumlah barang yang ingin ditambahkan: ";
+    cin >> jumlahBarang;
+
+    for (int i = 0; i < jumlahBarang; i++) {
+        system("cls");
+        judul();
+        cout << "\t\tBarang ke-" << i + 1 << endl;
+        daftarBarang[i].setBarang();
+        daftarBarang[i].setHarga();
+        totalHarga += daftarBarang[i].getHargaBarang() + daftarBarang[i].getHargaOngkir(); // Menambahkan harga barang ke totalHarga
+    }
 
 menu:
     system("cls");
     judul();
     pembeli.getPembeli();
     penjual.getPenjual();
-    cout << "\t\tNama Barang        : " << barang.getNamaBarang() << endl;
-    cout << "\t\tHarga Barang       : Rp. " << barang.getHargaBarang() << endl;
-    cout << "\t\tOngos Kirim        : Rp. " << barang.getHargaOngkir() << endl;
-    cout << "\t\t===========================================" << endl;
 
     cout << "\t\tMenu: " << endl;
     cout << "\t\t1. Pajak Lokal" << endl;
@@ -207,46 +215,55 @@ menu:
     cin >> pil;
     cout << endl;
 
-    if (pil == 1)
-    {
+    if (pil == 1) {
         system("cls");
         judul();
         pembeli.getPembeli();
         penjual.getPenjual();
-        pajak.getPajakLokal(barang);
-        cout <<"\t\tPress any key to continue...";
+        int totalKeseluruhan = totalHarga; // Menggunakan totalHarga sebagai total keseluruhan
+
+        for (int i = 0; i < jumlahBarang; i++) {
+            pajak.getPajakLokal(daftarBarang[i]);
+        }
+        cout << "\t\tTotal Keseluruhan: Rp. " << totalKeseluruhan << endl; // Menampilkan total keseluruhan
+        cout << "\n\t\tPress any key to continue...";
         getch();
     }
-    if (pil == 2)
-    {
+    else if (pil == 2) {
         system("cls");
+        int totalKeseluruhan = totalHarga; // Menggunakan totalHarga sebagai total keseluruhan
         judul();
         pembeli.getPembeli();
         penjual.getPenjual();
-        pajak.getPajakInter(barang);
-        cout <<"\t\tPress any key to continue...";
+
+        for (int i = 0; i < jumlahBarang; i++) {
+            pajak.getPajakInter(daftarBarang[i]);
+        }
+        cout << "\t\tTotal Keseluruhan: Rp. " << totalKeseluruhan << endl; // Menampilkan total keseluruhan
+        cout << "\n\t\tPress any key to continue...";
         getch();
     }
-    if (pil == 0)
-    {
-        cout <<"\t\tTerima kasih telah menggunakan program kami 🦄😉👍❤️"<<endl;
-        cout <<"\t\t--------------------------------------------------"<<endl;
-        cout <<"\t\t\t\tAnggota Kelompok"<<endl;
-        cout <<"\t\t--------------------------------------------------"<<endl;
-        cout <<"\t\t\t21081010001 Mochammad Fahmi Yasir"<<endl;
-        cout <<"\t\t\t21081010009 Cintya Juanita Elizabeth K."<<endl;
-        cout <<"\t\t\t21081010034 Jihan Octavia Salsabillah H."<<endl;
-        cout <<"\t\t\t21081010054 Adham Roy Bhafiel"<<endl;
-        cout <<"\t\t--------------------------------------------------"<<endl;
+    else if (pil == 0) {
+        cout << "\t\tTerima kasih telah menggunakan program kami " << endl;
+        cout << "\t\t--------------------------------------------------" << endl;
+        cout << "\t\t\t\tAnggota Kelompok" << endl;
+        cout << "\t\t--------------------------------------------------" << endl;
+        cout << "\t\t\t21081010001 Mochammad Fahmi Yasir" << endl;
+        cout << "\t\t\t21081010009 Cintya Juanita Elizabeth K." << endl;
+        cout << "\t\t\t21081010034 Jihan Octavia Salsabillah H." << endl;
+        cout << "\t\t\t21081010054 Adham Roy Bhafiel" << endl;
+        cout << "\t\t--------------------------------------------------" << endl;
         goto exit;
     }
-    else
-    {
+    else {
         system("cls");
         judul();
-        cout << "\t\tPilihan yang anda masukkan salah" << endl;
+        cout << "\t\tPilihan yang anda masukkan salah!" << endl;
+        cout << "\t\tPress any key to continue...";
+        getch();
         goto menu;
     }
+
 exit:
     return 0;
 }
